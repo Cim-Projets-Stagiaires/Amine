c93e760149b1e9332b4add73ee07e97aafbc180d
Mohamedamine88 <mohamedamineelidrissi88@gmail.com>
2024-08-06 22:51:12 +0100

    First commit

---
+++ Salles/delete.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_GET['id'])) {
+include("connect.php");
+$id = $_GET['id'];
+$sql = "DELETE FROM salles WHERE id='$id'";
+if(mysqli_query($conn,$sql)){
+    session_start();
+    $_SESSION["delete"] = "Salle supprimée!";
+    header("Location:index.php");
+}else{
+    die("Une erreur s'est produite.");
+}
+}else{
+    echo "Salle n'existe pas.";
+}
+?>
---
+++ Soutenances/delete.php
@@ -0,0 +1,25 @@
+<?php
+include('connect.php');
+
+$id = $_GET['id'];
+
+$sqlDeleteJury = "DELETE FROM membres_jury WHERE id_soutenance = $id";
+$resultJury = mysqli_query($conn, $sqlDeleteJury);
+
+if ($resultJury) {
+    $sqlDeleteSoutenance = "DELETE FROM soutenances WHERE id = $id";
+    $resultSoutenance = mysqli_query($conn, $sqlDeleteSoutenance);
+
+    if ($resultSoutenance) {
+        session_start();
+        $_SESSION['message'] = "Soutenance supprimée avec succès";
+        header("Location: index.php");
+    } else {
+        echo "Erreur lors de la suppression de la soutenance : " . mysqli_error($conn);
+    }
+} else {
+    echo "Erreur lors de la suppression des membres du jury : " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
